c951cf96e5e6319fb675b246093ceba3488b6e42
VishalKundu <kunduvishal35@gmail.com>
2023-02-07 21:33:49 +0530

    Page title changed to "KARMA | SHOP ...."

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = ({children}) => {
   return (
     <div className='layout'>
       <Head>
-        <title>JSM</title>
+        <title>KARMA | SHOP RITUALITIC AND SPIRITUAL GOODS</title>
       </Head>
       <header>
         <Navbar/>
